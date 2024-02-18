6b4ae0b1c73f010b1020b4d79808c5e3cdad61d6
tolerevo <tolerevo@hotmail.com>
2024-02-18 05:56:21 -0600

    mend

--- src/App.js
+++ src/App.js
@@ -13,7 +13,12 @@ import {
 // Add react-router-dom imports
 
 // create router with JSX Route elements
-const appRouter = createBrowserRouter(createRoutesFromElements(<Route path="/" element={<Root />} />));
+const appRouter = createBrowserRouter(createRoutesFromElements(
+<>
+  <Route path="/" element={<Root />} />
+  <Route path="/" element={<HomePage />} />
+  <Route path=":type" element={<HomePage />} />
+</>));
 
 function App() {
   return (

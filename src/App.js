21401ecd8fa3b56112caefd0b3e606b2eef469d1
tolerevo <tolerevo@hotmail.com>
2024-02-18 08:56:27 -0600

    fix searchinput

--- src/App.js
+++ src/App.js
@@ -13,19 +13,18 @@ import {
   Route,
 } from "react-router-dom";
 // create router with JSX Route elements
-const appRouter = createBrowserRouter(createRoutesFromElements(
-  <Route path="/" element={ <Root/> }>
-    <Route index element={<HomePage />}/>  
-    <Route path=":type" element={<HomePage />}/>
-    <Route path=":type/:id" element={<PetDetailsPage />}/>
-  </Route>
-))
-  
+const appRouter = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route index element={<HomePage />} />
+      <Route path=":type" element={<HomePage />} />
+      <Route path=":type/:id" element={<PetDetailsPage />} />
+    </Route>
+  )
+);
 
 function App() {
-  return (
-    <RouterProvider router={appRouter}/>
-  );
+  return <RouterProvider router={appRouter} />;
 }
 
 export default App;
